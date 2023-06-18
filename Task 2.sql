eceafab7e662e6de75d8d6215cc2ba47d7442c6a
StaceyKuzmenko <avasseikina@gmail.com>
2023-06-18 17:22:35 +0300

    Четвертая итерация

--- Task 2.sql
+++ Task 2.sql
@@ -1,22 +1,23 @@
 -- добавьте код сюда
 --Обновление статусов в представлении orders (из production.orderstatuslog)
 
-CREATE OR REPLACE VIEW analysis.orders AS 
-SELECT 	DISTINCT po.order_id,
-		po.order_ts,
-		po.user_id,
-		po.bonus_payment,
-		po.payment,
-		po."cost",
-		po.bonus_grant,
-		MAX(inc.status) AS status
-FROM (
-  	SELECT DISTINCT order_id,
-	  		 ROW_NUMBER() OVER (PARTITION BY order_id ORDER BY dttm ASC) AS last_date,
-	  		 MAX(status_id) AS status
-	FROM production.orderstatuslog posl
-  	GROUP BY order_id, status_id, dttm
-) AS inc
-RIGHT JOIN production.orders po ON inc.order_id=po.order_id
-GROUP BY po.order_id; 
+CREATE OR REPLACE VIEW analysis.orders AS  
+SELECT 	DISTINCT po.order_id, 
+		po.order_ts, 
+		po.user_id, 
+		po.bonus_payment, 
+		po.payment, 
+		po."cost", 
+		po.bonus_grant, 
+		inc.status 
+FROM ( 
+  	SELECT 	DISTINCT order_id, 
+			ROW_NUMBER() OVER (PARTITION BY order_id ORDER BY dttm DESC) AS last_date,
+			status_id as status
+  	FROM production.orderstatuslog posl 
+  	GROUP BY order_id, status_id, dttm 
+  	) AS inc 
+RIGHT JOIN production.orders po ON inc.order_id=po.order_id 
+WHERE inc.last_date = 1
+GROUP BY po.order_id, inc.status; 
 
